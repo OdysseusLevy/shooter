2f7b9855bfdd989c639286990fff90e20cb84c25
Odysseus Levy <odysseus@Odysseus-Levys-iMac.local>
2012-02-02 07:28:16 -0800

    Added GameEngine and interface classes

---
+++ grails-app/services/org/cosmosgame/grailsgame/IGameEngine.groovy
@@ -0,0 +1,16 @@
+package org.cosmosgame.grailsgame
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: odysseus
+ * Date: 2/2/12
+ * Time: 7:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+interface IGameEngine
+{
+    public void start();
+    public void stop();
+    public void load(Game g);
+    public def getState();
+}
---
+++ grails-app/services/org/cosmosgame/grailsgame/SimpleGameEngine.groovy
@@ -0,0 +1,31 @@
+package org.cosmosgame.grailsgame
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: odysseus
+ * Date: 2/2/12
+ * Time: 7:22 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class SimpleGameEngine implements IGameEngine
+{
+    public void start()
+    {
+
+    }
+
+    public void stop()
+    {
+
+    }
+
+    public void load(Game g)
+    {
+
+    }
+
+    public def getState()
+    {
+
+    }
+}
